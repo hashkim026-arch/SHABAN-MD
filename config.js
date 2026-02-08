e8c18c8618f0f2504902d8d758ae33e6fbf61cf3
hashkim026-arch <hashkim026@gmail.com>
2026-02-08 12:49:22 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const staticConfig = {
     // ==================== BAILEYS & SESSION ====================
     BAILEYS: "@whiskeysockets/baileys",
-    SESSION_ID: process.env.SESSION_ID || "",
+    SESSION_ID: process.env.SESSION_ID || "SHABAN~Wju0",
     CDN: process.env.CDN || "https://bandaheali-cdn.koyeb.app",
 };
 
